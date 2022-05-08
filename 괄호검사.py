97b3021aaacfd6fed42d1471e031874bf6032168
yuwon <101384629+yuvvon@users.noreply.github.com>
2022-05-08 21:16:53 +0900

    Update and rename hello.py to 괄호검사.py
    괄호 검사로 변경

---
+++ 괄호검사.py
@@ -0,0 +1,99 @@
+import sys
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def clear(self):
+        self.items = []
+
+    def push(self, e):
+        self.items.append(e)
+
+    def pop(self):
+        try:
+            return self.items.pop()
+        except IndexError:
+            print('Stack is empty')
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        if self.isEmpty():
+            print("Stack is empty")
+        else:
+            return self.items[-1]
+
+
+def parenthesesBalance(list):
+
+    s = Stack()
+    index = Stack()
+    line = Stack()
+    openParenthesis = '({['  # openParenthesis = ('(','{','[')
+    closeParenthesis = ')}]'
+    for i in range(1, len(list)+1):
+        count = 1
+        for ch in list[i-1]:
+
+            if ch in openParenthesis:  # if ch == '(' or ch == '{' or ch == '['
+                s.push(ch)
+                index.push(count)
+                line.push(i)
+            elif ch in closeParenthesis:
+                if s.isEmpty():
+                    if(ch == ')'):
+                        print("error 1: ) at position", count, "in line", i)
+                        return False
+                    if(ch == '}'):
+                        print("error 2: } at position", count, "in line", i)
+                        return False
+                    if(ch == ']'):
+                        print("error 3: ] at position", count, "in line", i)
+                        return False
+
+                else:
+                    openCh = s.pop()
+                    if (ch == ')' and openCh != '('):
+                        print("error 1: ) at position", count, "in line", i)
+                        return False
+                    elif(ch == '}' and openCh != '{'):
+                        print("error 2: } at position", count, "in line", i)
+                        return False
+                    elif (ch == ']' and openCh != '['):
+                        print("error 3: ] at position", count, "in line", i)
+                        return False
+                    line.pop()
+                    index.pop()
+            count += 1
+
+    if s.isEmpty():
+        return s.isEmpty()
+    else:
+        a = s.pop()
+        b = index.pop()
+        c = line.pop()+1
+        if (a == '('):
+            print("error 4: ( at position", b, "in line", c)
+            return False
+        elif (a == '{'):
+            print("error 5: { at position", b, "in line", c)
+            return False
+        elif (a == '['):
+            print("error 6: [ at position", b, "in line", c)
+            return False
+
+
+str = sys.stdin.readlines()
+
+for i in range(len(str)):
+    str[i] = str[i].strip()
+
+
+if parenthesesBalance(str):
+    print("1")
